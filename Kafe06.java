79e6b8e346f0f5e5c4d92e1dc25b68195348d6f1
Irdgyu <gilangbayuirwana@gmail.com>
2024-11-29 18:43:41 +0700

    fourth commit

--- Kafe06.java
+++ Kafe06.java
@@ -1,11 +1,18 @@
 public class Kafe06 {
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Gilang", true, "DISKON50");
     }
-    public static void Menu(String namaPelanggan,boolean isMember) {
+    public static void Menu(String namaPelanggan,boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
+            System.out.println("WAH, Anda berhak mendapatkan diskon tambahan sebesar 50%. Selamat !!!");
+        }else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+            System.out.println("WAH, Anda berhak mendapatkan diskon tambahan sebesar 30%. Selamat !!!");
+        }else {
+            System.out.println("Mohon maaf, kode invalid :(");
         }
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp.15000");
@@ -15,7 +22,6 @@ public class Kafe06 {
         System.out.println("5. Roti Bakar - Rp.10000");
         System.out.println("6. Mie Goreng- Rp.18000");
         System.out.println("=========================");
-        System.out.println("Silahkan pilih menu ynag anda inginkan :)");
+        System.out.println("Silahkan pilih menu yaNg anda inginkan :)");
     }
-
 }
