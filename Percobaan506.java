7d2e5ca927ffc9d39aab81884dc26ec2a9a2d98f
Irdgyu <gilangbayuirwana@gmail.com>
2024-11-29 22:05:40 +0700

    second commit

--- Percobaan506.java
+++ Percobaan506.java
@@ -9,9 +9,17 @@ public class Percobaan506 {
         l = input6.nextInt();
         System.out.print("Masukkan tinggi: ");
         t = input6.nextInt();
-        L = p * l;
+        L = hitungLuas(p,l);
         System.out.println("Luas persegi panjang adalah: " + L);
-        vol = p * l * t;
+        vol = hitungVolume(t,p,l);
         System.out.println("Volume balok adalah: " + vol);
+    }
+    public static int hitungLuas(int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+    public static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungLuas(a,b) * tinggi;
+        return volume;
     }
 }
