db3efc13c11bb3aa64e7ce11806baf0467c2a16c
Irdgyu <gilangbayuirwana@gmail.com>
2024-11-29 21:28:16 +0700

    first commit

---
+++ PengunjungKafe06.java
@@ -0,0 +1,10 @@
+public class PengunjungKafe06 {
+    public static void main(String[] args) {
+        daftarPengunjung("Gilang", "Rifky", "Hanif");
+    }
+    public static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++)
+            System.out.println("- " + namaPengunjung[i]);
+    }
+}
