987ad6e50025a61e2d94cb35fa622da2077f41b0
Irdgyu <gilangbayuirwana@gmail.com>
2024-11-29 18:30:15 +0700

    first commit

---
+++ Kafe06.java
@@ -0,0 +1,17 @@
+public class Kafe06 {
+    public static void main(String[] args) {
+        Menu();
+    }
+    public static void Menu() {
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. Kopi Hitam - Rp.15000");
+        System.out.println("2. Cappuccino - Rp.20000");
+        System.out.println("3. Latte - Rp.22000");
+        System.out.println("4. Teh Tarik - Rp.12000");
+        System.out.println("5. Roti Bakar - Rp.10000");
+        System.out.println("6. Mie Goreng- Rp.18000");
+        System.out.println("=========================");
+        System.out.println("Silahkan pilih menu ynag anda inginkan :)");
+        
+    }
+}
