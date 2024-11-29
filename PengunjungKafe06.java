4032e826080ea4ed4b4fb6b5502a202afa7f4daa
Irdgyu <gilangbayuirwana@gmail.com>
2024-11-29 21:35:52 +0700

    second commit

--- PengunjungKafe06.java
+++ PengunjungKafe06.java
@@ -4,7 +4,8 @@ public class PengunjungKafe06 {
     }
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++)
+        for (int i = 0; i < namaPengunjung.length; i++) {
             System.out.println("- " + namaPengunjung[i]);
+        }
     }
 }
